39f62fdf0ad3e8a39650b98e3f18563c56440a69
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-08 13:35:29 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 8;
+    cout << x << endl;
+    x = 9;
+    cout << x << endl;
+    x = x + 3;
+    cout << x << endl;
+    return 0;
+}
