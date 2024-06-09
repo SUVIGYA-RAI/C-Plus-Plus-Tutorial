1d33c783cc8ce158f41974af29825adf9f456172
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-09 12:08:33 +0530

    Add files via upload

---
+++ float.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a = 4;
+    cout << a << endl;
+    float x = 10.8, y = 2.4;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << y / x << endl;
+}
