e156e0335d2888ea23ab5f8054b178df744a5c3a
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-11 20:50:54 +0530

    Add files via upload

---
+++ arithmeticoperator.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 4;
+//     cout << x << endl;
+//     x = 8;
+//     cout << x << endl;
+//     x = x + 2;
+//     cout << x << endl;
+//     x -= 8;
+//     cout << x <<endl;
+//     x *= 4;
+//     cout << x << endl;
+//     x /= 2;
+//     cout << x << endl;
+// }
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 2, y = 5;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << y / x << endl;
+}
