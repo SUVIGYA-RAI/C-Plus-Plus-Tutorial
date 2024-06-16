bc9ba5e8f847beabb8f6d7cc9c409914bd481371
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-16 23:25:38 +0530

    Add files via upload

---
+++ areaOfcircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float area;
+    float pi = 3.14159265359;
+    int x = 4;
+    area = pi * x * x;
+    cout << "Area is : "<< area << endl; 
+    
+}
