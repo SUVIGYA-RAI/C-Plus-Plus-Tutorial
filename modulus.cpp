9c9bec5ad0721e1545a17dc134d53ec16653d909
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-19 13:58:36 +0530

    Add files via upload

---
+++ modulus.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 10;
+    int y = -3;
+    cout << "The remainder is : " << x % y;
+}
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 2;
+//     int y = 8;                 
+//     //the output will be 2 because it cannot take value in decimals and wouldn't return 0.25
+//     cout << "The remainder is : " << x % y;
+// }
