9d922fed8648d51aadb496ab54e65c43e95bc92f
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-06-28 22:18:50 +0530

    Add files via upload

---
+++ Typecasting.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin >> x;
+//     float y = (float)x;       //Typecasting
+//     cout << y / 2;
+// }
+
+//to print ASCII values
+#include <iostream>
+using namespace std;
+int main(){
+    char s;
+    cin >> s;
+    cout << (int)s;
+}
