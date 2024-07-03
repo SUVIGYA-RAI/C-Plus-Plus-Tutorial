b702a3a075be489122c73bced85f2eb40ba5a2ed
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-07-03 23:09:08 +0530

    Add files via upload

---
+++ 1To100.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i = 1; i <= 100; i++){
+//         cout << i << " " ;
+//     }
+// }
+
+
+// Print all the even numbers from 1 To 100
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i = 1; i <= 100; i++){
+//         if(i%2==0){
+//             cout << i << " ";
+//         }
+//     }
+/*
+      A Better Method
+      for (int i = 2; i <= 100; i=i+2){
+      cout << i << " ";
+      }
+*/
+// }
+
+// Print all the odd numbers from 1 To 100
+#include <iostream>
+using namespace std;
+int main(){
+      for (int i = 1; i <= 99; i=i+2){
+        cout << i << " ";
+      }
+}
