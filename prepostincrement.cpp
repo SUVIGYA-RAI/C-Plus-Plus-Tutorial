bba3022e0da6a63ec8b33f5133ac68e9a7956f74
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2024-07-09 23:51:51 +0530

    Add files via upload

---
+++ prepostincrement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout << x << endl;
+    cout << ++x << endl;
+    cout << x ;
+}
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     cout << x << endl;
+//     cout << x++ << endl;
+//     cout << x ;
+// }
